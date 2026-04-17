fb438ecaf1f3f848b47838468536136ca62a96e1
Guofeng Luo <56261249+luoguofeng0401@users.noreply.github.com>
2026-04-18 02:03:15 +0800

    Add files via upload

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Guofeng Luo on 2026-04-17.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
