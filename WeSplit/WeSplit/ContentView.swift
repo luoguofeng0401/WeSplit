63f2d8438e9d08d4a101d49257b5eeee1b481ed4
Guofeng Luo <56261249+luoguofeng0401@users.noreply.github.com>
2026-05-07 10:07:16 +0800

    Add files via upload

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -65,6 +65,7 @@ struct ContentView: View {
                 
                 Section("Total amount") {
                     Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundStyle(tipPercentage == 0 ? .red : .white)
                 }
                 
             }
